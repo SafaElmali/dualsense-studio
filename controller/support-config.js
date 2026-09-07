21ae4a1834aaae194e184af048e7c34b0c893390
safaelmali <tsafaelmali@gmail.com>
2026-09-07 16:35:14 +0300

    fix: Point support links to Safa's creator page

--- controller/support-config.js
+++ controller/support-config.js
@@ -1,4 +1,4 @@
 // Only publish verified creator pages. Empty values keep that option hidden.
 export const supportUrls = {
-  buymeacoffee: 'https://buymeacoffee.com/dualsensestudio',
+  buymeacoffee: 'https://buymeacoffee.com/safaelmali',
 };
